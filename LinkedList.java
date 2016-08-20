39df65d56b0d7807fbe49d3b0b9175cdb6c1adbc
cindygao93 <1zxg@queensu.ca>
2016-08-20 01:21:07 -0400

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -3,10 +3,6 @@
  COMP1406/1006 - Fall 2013
  Assignment 9 - Problem 1
 
-Name: Cindy Gao
-Student Number: 100861300
-Date: November 25, 2013
-
  <p>
  A class that creates a linked list and methods which can operate on the linked list.
 */
